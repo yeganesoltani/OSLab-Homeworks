0686879f207f2ade54ed15455088b43bfe7dc1de
yeganesoltani <soltani.yegane78@gmail.com>
2020-12-23 02:23:56 -0800

    1

---
+++ OS-9/1.sh
@@ -0,0 +1,2 @@
+yegane@ubuntu:~/Music/OSLab-Homeworks/OS-9 git config --global user.email "soltani.yegane78@gmail.com"
+yegane@ubuntu:~/Music/OSLab-Homeworks/OS-9 git config --global user.name "yeganesoltani"
