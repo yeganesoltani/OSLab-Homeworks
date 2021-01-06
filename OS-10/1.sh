be757927b76628937b81f6e7bbaf027fdf0ead36
yeganesoltani <soltani.yegane78@gmail.com>
2021-01-06 11:04:02 -0800

    .

--- OS-10/1.sh
+++ OS-10/1.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-echo '*MERRY CHRISTMAS*'
-echo 'Please enter your age:'
-read $1
+echo "*MERRY CHRISTMAS*"
+echo "Please enter your age"
 if [ $1 -ge 18 ]
 then
-   echo 'You may go to the party.'
-else  [ "$1"  -lt 18 ]
-   echo 'Do you have letter of confirmation of your parents?(Y/N)'
-read $2
-if [ $2 == 'Y' ]
+   echo "You may go to the party."
+else  [ $1  -lt 18 ]
+   echo "Do you have letter of confirmation of your parents?(Y/N)"
+if [ $2 = "Y" ]
 then 
-   echo 'You may go to the party but be back before midnight.'
-fi
+   echo "You may go to the party but be back before midnight."
 else
-   echo 'You may not go to the party.'
+   echo "You may not go to the party."
+fi
 fi
 
