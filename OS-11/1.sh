e496d1bc001880f27f1479ca50fbaa8bd4545cd1
yeganesoltani <soltani.yegane78@gmail.com>
2021-01-13 00:45:33 -0800

    .

---
+++ OS-11/1.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+echo "-----Calculator-----"
+echo "choose your option"
+echo "Press (+) For Sum , (-) For Sub ,(*) For Multiply , (/) For Division"
+read operation 
+echo "Enter first number "
+read A
+echo "Enter second number"
+read B
+echo "THE RESULT IS :"
+if [ $operation = "+"]
+then
+echo $(($A+$B))
+elif [ $operation = "-"]
+then
+echo $(($A-$B))
+elif [ $operation = "*"]
+then
+echo $(($A*$B))
+elif [ $operation = "/"]
+then
+echo $(($A/$B))
+fi
---
+++ OS-11/README.md
@@ -0,0 +1,4 @@
+# OSLab-Homeworks
+operation & A & B are the variables
+echo for print
+first you choose your item after enter your numbers at last you see the result
